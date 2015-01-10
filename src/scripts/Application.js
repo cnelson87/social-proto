623095c551fc7e3303958800d4854d844d3bb842
Chris Nelson <cnelson87@gmail.com>
2015-01-09 16:10:47 -0800

    bug fixes and updates, convert app to widget

---
+++ src/scripts/Application.js
@@ -0,0 +1,25 @@
+/**
+ * Application Module
+ * 
+ * @author Chris Nelson
+ */
+
+var AppConfig				= require('config/AppConfig');
+var SocialWall				= require('widgets/SocialWall');
+//var templateSocialItems 	= require('../templates/social-items.hbs');
+
+
+var Application = {
+
+	initialize: function() {
+		//console.log('Application:initialize');
+
+		if (AppConfig.isIE9) {this.$html.addClass('ie9');}
+
+		new SocialWall($('#social-app'), {dataUrl: AppConfig.dataUrl, dataLimit: AppConfig.dataLimit});
+
+	}
+
+};
+
+module.exports = Application;
